20bcd95f0678cef47b814a13b733106e8a7937a8
Deepak-Ragavendra <deeppanbhu05@gmail.com>
2026-02-02 05:49:05 +0530

    Practice

--- Lab-4/client.c
+++ Lab-4/client.c
@@ -1,76 +1,58 @@
-#include<stdio.h>
-#include<Winsock2.h>
-#include<time.h>
-#pragma comment (lib,"Ws2_32.lib")
+#include <stdio.h>
+#include <winsock2.h>
+#include <time.h>
+#pragma comment(lib,"Ws2_32.lib")
 
-void left_shift(int key[10],int array[5],int k){
+void left_shift(int key[10], int array[5], int k){
     if(k==0){
-        int t=key[0];
-        for(int i=1;i<5;i++){
-            array[i-1]=key[i];
-        }
-        array[4]=t;
-    }
-    else{
-        int t=key[5];
-        for(int i=6;i<10;i++){
-            array[i-6]=key[i];
-        }
-        array[4]=t;
+        int t = key[0];
+        for(int i=1;i<5;i++) array[i-1] = key[i];
+        array[4] = t;
+    } else {
+        int t = key[5];
+        for(int i=6;i<10;i++) array[i-6] = key[i];
+        array[4] = t;
     }
 }
 
-int main() {
-    clock_t start = clock();
+int main(){
     WSADATA wsadata;
-    SOCKET socketstatus;
-    struct sockaddr_in clientSocket;
+    SOCKET sock;
+    struct sockaddr_in server;
+    WSAStartup(MAKEWORD(2,2), &wsadata);
 
-    //Winsock init
-    if(WSAStartup(MAKEWORD(2,2), &wsadata)!=0){
-        printf("WSAStartup Failed\n");
-        return 0;
-    }
-
-    //Key generation
+    // -------- KEY GENERATION --------
     int P10[]={3,5,2,7,4,10,1,9,8,6};
     int P8[]={6,3,7,4,8,5,10,9};
     int key[]={1,0,1,0,0,0,0,0,1,0};
-    int temp[10],L[5],R[5];
-
-    for(int i=0;i<10;i++){
-        temp[i]=key[P10[i]-1];
-    }
-
+    int temp[10], L[5], R[5], newkey[10];
+    for(int i=0;i<10;i++) temp[i]=key[P10[i]-1];
     left_shift(temp,L,0);
     left_shift(temp,R,1);
-
-    int newkey[10];
-    for(int i=0;i<5;i++){
-        newkey[i]=L[i];
-        newkey[i+5]=R[i];
-    }
-
+    for(int i=0;i<5;i++){ newkey[i]=L[i]; newkey[i+5]=R[i]; }
     int k1[8];
-    for(int i=0;i<8;i++){
-        k1[i]=newkey[P8[i]-1];
-    }
-
-    left_shift(L,L,0);
-    left_shift(R,R,1);
-    left_shift(L,L,0);
-    left_shift(R,R,1);
-
+    for(int i=0;i<8;i++) k1[i]=newkey[P8[i]-1];
+    left_shift(L,L,0); left_shift(R,R,1);
+    left_shift(L,L,0); left_shift(R,R,1);
+    for(int i=0;i<5;i++){ newkey[i]=L[i]; newkey[i+5]=R[i]; }
     int k2[8];
-    for(int i=0;i<5;i++){
-        newkey[i]=L[i];
-        newkey[i+5]=R[i];
-    }
-    for(int i=0;i<8;i++){
-        k2[i]=newkey[P8[i]-1];
-    }
-    printf("Keys Generated Successfully\n");
-    //Encryption
+    for(int i=0;i<8;i++) k2[i]=newkey[P8[i]-1];
+
+    // -------- INPUT PLAINTEXT --------
+    int pt_len;
+    printf("Enter plaintext length (bits): ");
+    scanf("%d",&pt_len);
+
+    int PT[128];
+    printf("Enter plaintext bits:\n");
+    for(int i=0;i<pt_len;i++) scanf("%d",&PT[i]);
+
+    // -------- PADDING --------
+    int pad = (8 - pt_len % 8) % 8;
+    int padded_len = pt_len + pad;
+    for(int i=pt_len;i<padded_len;i++) PT[i]=0;
+
+    // -------- SDES TABLES --------
     int S0[4][4]={{1,0,3,2},{3,2,1,0},{0,2,1,3},{3,1,3,2}};
     int S1[4][4]={{0,1,2,3},{2,0,1,3},{3,0,1,0},{2,1,0,3}};
     int IP[8]={2,6,3,1,4,8,5,7};
@@ -78,115 +60,60 @@ int main() {
     int P4[4]={2,4,3,1};
     int IP_inv[8]={4,1,3,5,7,2,8,6};
 
-    int PT[8];
-    printf("Enter the plain text of 8 bit:\n");
-    for(int i=0;i<8;i++){
-        scanf("%d",&PT[i]);
+    int cipher[128], ci=0;
+
+    for(int b=0;b<padded_len;b+=8){
+        int block[8], IP_out[8], L4[4], R4[4];
+        for(int i=0;i<8;i++) block[i]=PT[b+i];
+        for(int i=0;i<8;i++) IP_out[i]=block[IP[i]-1];
+        for(int i=0;i<4;i++){ L4[i]=IP_out[i]; R4[i]=IP_out[i+4]; }
+
+        int EP_out[8], xor_k[8], NL[4], NR[4];
+        for(int i=0;i<8;i++) EP_out[i]=R4[EP[i]-1];
+        for(int i=0;i<8;i++){
+            xor_k[i]=k1[i]^EP_out[i];
+            if(i<4) NL[i]=xor_k[i];
+            else NR[i-4]=xor_k[i];
+        }
+
+        int r1=NL[0]*2+NL[3], c1=NL[1]*2+NL[2];
+        int r2=NR[0]*2+NR[3], c2=NR[1]*2+NR[2];
+        int s_out[4]={S0[r1][c1]/2,S0[r1][c1]%2,S1[r2][c2]/2,S1[r2][c2]%2};
+        int p4[4];
+        for(int i=0;i<4;i++) p4[i]=s_out[P4[i]-1]^L4[i];
+        for(int i=0;i<4;i++){ L4[i]=R4[i]; R4[i]=p4[i]; }
+
+        for(int i=0;i<8;i++) EP_out[i]=R4[EP[i]-1];
+        for(int i=0;i<8;i++){
+            xor_k[i]=k2[i]^EP_out[i];
+            if(i<4) NL[i]=xor_k[i];
+            else NR[i-4]=xor_k[i];
+        }
+
+        r1=NL[0]*2+NL[3]; c1=NL[1]*2+NL[2];
+        r2=NR[0]*2+NR[3]; c2=NR[1]*2+NR[2];
+        s_out[0]=S0[r1][c1]/2; s_out[1]=S0[r1][c1]%2;
+        s_out[2]=S1[r2][c2]/2; s_out[3]=S1[r2][c2]%2;
+        for(int i=0;i<4;i++) p4[i]=s_out[P4[i]-1]^L4[i];
+
+        int pre[8];
+        for(int i=0;i<4;i++){ pre[i]=p4[i]; pre[i+4]=R4[i]; }
+        for(int i=0;i<8;i++) cipher[ci++]=pre[IP_inv[i]-1];
     }
 
-    //Initial Permutation
-    int IP_out[8];
-    for(int i=0;i<8;i++){
-        IP_out[i]=PT[IP[i]-1];
-    }
+    // -------- SOCKET SEND --------
+    sock=socket(AF_INET,SOCK_STREAM,0);
+    server.sin_family=AF_INET;
+    server.sin_port=htons(4000);
+    server.sin_addr.S_un.S_addr=inet_addr("127.0.0.1");
+    connect(sock,(struct sockaddr*)&server,sizeof(server));
 
-    int L4[4],R4[4];
-    for(int i=0;i<4;i++){
-        L4[i]=IP_out[i];
-        R4[i]=IP_out[i+4];
-    }
+    send(sock,(char*)&pt_len,sizeof(int),0);
+    send(sock,(char*)&padded_len,sizeof(int),0);
+    send(sock,(char*)cipher,padded_len*sizeof(int),0);
 
-    //Round 1
-    int new_EP[8],xor_k[8],NL[4],NR[4];
-    for(int i=0;i<8;i++) new_EP[i]=R4[EP[i]-1];
-
-    for(int i=0;i<8;i++){
-        xor_k[i]=k1[i]^new_EP[i];
-        if(i<4) NL[i]=xor_k[i];
-        else NR[i-4]=xor_k[i];
-    }
-
-    int r1=NL[0]*2+NL[3], c1=NL[1]*2+NL[2];
-    int r2=NR[0]*2+NR[3], c2=NR[1]*2+NR[2];
-
-    int s_out[4]={S0[r1][c1]/2,S0[r1][c1]%2,S1[r2][c2]/2,S1[r2][c2]%2};
-
-    int p4[4];
-    for(int i=0;i<4;i++) p4[i]=s_out[P4[i]-1];
-    for(int i=0;i<4;i++) p4[i]^=L4[i];
-
-    //Swap
-    for(int i=0;i<4;i++){
-        L4[i]=R4[i];
-        R4[i]=p4[i];
-    }
-
-    //Round 2
-    for(int i=0;i<8;i++) new_EP[i]=R4[EP[i]-1];
-    for(int i=0;i<8;i++){
-        xor_k[i]=k2[i]^new_EP[i];
-        if(i<4) NL[i]=xor_k[i];
-        else NR[i-4]=xor_k[i];
-    }
-
-    r1=NL[0]*2+NL[3]; c1=NL[1]*2+NL[2];
-    r2=NR[0]*2+NR[3]; c2=NR[1]*2+NR[2];
-
-    s_out[0]=S0[r1][c1]/2; s_out[1]=S0[r1][c1]%2;
-    s_out[2]=S1[r2][c2]/2; s_out[3]=S1[r2][c2]%2;
-
-    for(int i=0;i<4;i++) p4[i]=s_out[P4[i]-1];
-    for(int i=0;i<4;i++) p4[i]^=L4[i];
-
-    int pre_cipher[8];
-    for(int i=0;i<4;i++){
-        pre_cipher[i]=p4[i];
-        pre_cipher[i+4]=R4[i];
-    }
-
-    int cipher[8];
-    for(int i=0;i<8;i++){
-        cipher[i]=pre_cipher[IP_inv[i]-1];
-    }
-
-    //Cipher Ready
-    printf("Cipher Text: ");
-    for(int i=0;i<8;i++) printf("%d",cipher[i]);
-    printf("\n");
-
-    //Socket send
-    socketstatus = socket(AF_INET, SOCK_STREAM, 0);
-    if(socketstatus == INVALID_SOCKET){
-        printf("Socket creation failed\n");
-        WSACleanup();
-        return 0;
-    }
-
-    clientSocket.sin_family = AF_INET;
-    clientSocket.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
-    clientSocket.sin_port = htons(4000);
-
-    if(connect(socketstatus,(struct sockaddr*)&clientSocket,sizeof(clientSocket)) == SOCKET_ERROR){
-        printf("Connection Failed\n");
-        closesocket(socketstatus);
-        WSACleanup();
-        return 0;
-    }
-
-    printf("Connected to Server\n");
-
-    int size = 8;
-    send(socketstatus,(char*)&size,sizeof(int),0);
-
-    send(socketstatus,(char*)cipher,8*sizeof(int),0);
-
-    printf("Cipher Sent Successfully\n");
-
-    closesocket(socketstatus);
+    printf("Cipher sent successfully\n");
+    closesocket(sock);
     WSACleanup();
-    clock_t end = clock();
-    double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-
-    printf("Time taken for computation: %f seconds\n", cpu_time_used);
     return 0;
 }
--- Lab-4/server.c
+++ Lab-4/server.c
@@ -1,109 +1,60 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <winsock2.h>
-#include <time.h>
 #pragma comment(lib,"Ws2_32.lib")
 
 void left_shift(int key[10], int array[5], int k){
     if(k==0){
-        int t = key[0];
-        for(int i=1;i<5;i++){
-            array[i-1] = key[i];
-        }
-        array[4] = t;
-    }
-    else{
-        int t = key[5];
-        for(int i=6;i<10;i++){
-            array[i-6] = key[i];
-        }
-        array[4] = t;
+        int t=key[0];
+        for(int i=1;i<5;i++) array[i-1]=key[i];
+        array[4]=t;
+    } else {
+        int t=key[5];
+        for(int i=6;i<10;i++) array[i-6]=key[i];
+        array[4]=t;
     }
 }
 
 int main(){
-    clock_t start=clock();
     WSADATA wsadata;
-    SOCKET socketStatus, acceptConnection;
-    struct sockaddr_in serverSocket, clientSocket;
-    int clientSize = sizeof(clientSocket);
+    SOCKET server, client;
+    struct sockaddr_in addr, cli;
+    int len=sizeof(cli);
+    WSAStartup(MAKEWORD(2,2),&wsadata);
 
-    //Winsock Init
-    WSAStartup(MAKEWORD(2,2), &wsadata);
-
-    socketStatus = socket(AF_INET, SOCK_STREAM, 0);
-    if(socketStatus == INVALID_SOCKET){
-        printf("Socket creation failed\n");
-        return 0;
-    }
-
-    serverSocket.sin_family = AF_INET;
-    serverSocket.sin_addr.S_un.S_addr = INADDR_ANY;
-    serverSocket.sin_port = htons(4000);
-
-    bind(socketStatus, (struct sockaddr*)&serverSocket, sizeof(serverSocket));
-    listen(socketStatus, 3);
-
+    server=socket(AF_INET,SOCK_STREAM,0);
+    addr.sin_family=AF_INET;
+    addr.sin_port=htons(4000);
+    addr.sin_addr.S_un.S_addr=INADDR_ANY;
+    bind(server,(struct sockaddr*)&addr,sizeof(addr));
+    listen(server,1);
     printf("Waiting for client...\n");
-    acceptConnection = accept(socketStatus, (struct sockaddr*)&clientSocket, &clientSize);
-    printf("Client Connected!\n");
+    client=accept(server,(struct sockaddr*)&cli,&len);
 
-    //Receive cipher size
-    int cipherSize;
-    recv(acceptConnection, (char*)&cipherSize, sizeof(int), 0);
+    int original_len,padded_len;
+    recv(client,(char*)&original_len,sizeof(int),0);
+    recv(client,(char*)&padded_len,sizeof(int),0);
 
-    //Receive cipher
-    int *cipher = (int*)malloc(cipherSize * sizeof(int));
-    recv(acceptConnection, (char*)cipher, cipherSize * sizeof(int), 0);
+    int cipher[128];
+    recv(client,(char*)cipher,padded_len*sizeof(int),0);
 
-    printf("Received Cipher Text: ");
-    for(int i=0;i<cipherSize;i++){
-        printf("%d", cipher[i]);
-    }
-    printf("\n");
-
-    //KEY GENERATION
+    // -------- KEY GENERATION (same as client) --------
     int P10[]={3,5,2,7,4,10,1,9,8,6};
     int P8[]={6,3,7,4,8,5,10,9};
     int key[]={1,0,1,0,0,0,0,0,1,0};
-
-    int temp[10], Lk[5], Rk[5];
-
-    for(int i=0;i<10;i++){
-        temp[i] = key[P10[i]-1];
-    }
-
-    left_shift(temp, Lk, 0);
-    left_shift(temp, Rk, 1);
-
-    int newkey[10];
-    for(int i=0;i<5;i++){
-        newkey[i] = Lk[i];
-        newkey[i+5] = Rk[i];
-    }
-
+    int temp[10], L[5], R[5], newkey[10];
+    for(int i=0;i<10;i++) temp[i]=key[P10[i]-1];
+    left_shift(temp,L,0);
+    left_shift(temp,R,1);
+    for(int i=0;i<5;i++){ newkey[i]=L[i]; newkey[i+5]=R[i]; }
     int k1[8];
-    for(int i=0;i<8;i++){
-        k1[i] = newkey[P8[i]-1];
-    }
-
-    //Second key
-    left_shift(Lk, Lk, 0);
-    left_shift(Rk, Rk, 1);
-    left_shift(Lk, Lk, 0);
-    left_shift(Rk, Rk, 1);
-
-    for(int i=0;i<5;i++){
-        newkey[i] = Lk[i];
-        newkey[i+5] = Rk[i];
-    }
-
+    for(int i=0;i<8;i++) k1[i]=newkey[P8[i]-1];
+    left_shift(L,L,0); left_shift(R,R,1);
+    left_shift(L,L,0); left_shift(R,R,1);
+    for(int i=0;i<5;i++){ newkey[i]=L[i]; newkey[i+5]=R[i]; }
     int k2[8];
-    for(int i=0;i<8;i++){
-        k2[i] = newkey[P8[i]-1];
-    }
+    for(int i=0;i<8;i++) k2[i]=newkey[P8[i]-1];
 
-    //DECRYPTION
+    // -------- DECRYPT --------
     int S0[4][4]={{1,0,3,2},{3,2,1,0},{0,2,1,3},{3,1,3,2}};
     int S1[4][4]={{0,1,2,3},{2,0,1,3},{3,0,1,0},{2,1,0,3}};
     int IP[8]={2,6,3,1,4,8,5,7};
@@ -111,82 +62,53 @@ int main(){
     int P4[4]={2,4,3,1};
     int IP_inv[8]={4,1,3,5,7,2,8,6};
 
-    //Initial Permutation
-    int IP_out[8];
-    for(int i=0;i<8;i++){
-        IP_out[i] = cipher[IP[i]-1];
-    }
+    int PT[128], pi=0;
 
-    int L4[4], R4[4];
-    for(int i=0;i<4;i++){
-        L4[i] = IP_out[i];
-        R4[i] = IP_out[i+4];
-    }
+    for(int b=0;b<padded_len;b+=8){
+        int block[8], IP_out[8], L4[4], R4[4];
+        for(int i=0;i<8;i++) block[i]=cipher[b+i];
+        for(int i=0;i<8;i++) IP_out[i]=block[IP[i]-1];
+        for(int i=0;i<4;i++){ L4[i]=IP_out[i]; R4[i]=IP_out[i+4]; }
 
-    //fk with k2
-    int new_EP[8], xor_k[8], NL[4], NR[4];
+        int EP_out[8], xor_k[8], NL[4], NR[4];
+        for(int i=0;i<8;i++) EP_out[i]=R4[EP[i]-1];
+        for(int i=0;i<8;i++){
+            xor_k[i]=k2[i]^EP_out[i];
+            if(i<4) NL[i]=xor_k[i];
+            else NR[i-4]=xor_k[i];
+        }
 
-    for(int i=0;i<8;i++) new_EP[i] = R4[EP[i]-1];
-    for(int i=0;i<8;i++){
-        xor_k[i] = k2[i] ^ new_EP[i];
-        if(i<4) NL[i]=xor_k[i];
-        else NR[i-4]=xor_k[i];
-    }
+        int r1=NL[0]*2+NL[3], c1=NL[1]*2+NL[2];
+        int r2=NR[0]*2+NR[3], c2=NR[1]*2+NR[2];
+        int s_out[4]={S0[r1][c1]/2,S0[r1][c1]%2,S1[r2][c2]/2,S1[r2][c2]%2};
+        int p4[4];
+        for(int i=0;i<4;i++) p4[i]=s_out[P4[i]-1]^L4[i];
+        for(int i=0;i<4;i++){ L4[i]=R4[i]; R4[i]=p4[i]; }
 
-    int r1=NL[0]*2+NL[3], c1=NL[1]*2+NL[2];
-    int r2=NR[0]*2+NR[3], c2=NR[1]*2+NR[2];
+        for(int i=0;i<8;i++) EP_out[i]=R4[EP[i]-1];
+        for(int i=0;i<8;i++){
+            xor_k[i]=k1[i]^EP_out[i];
+            if(i<4) NL[i]=xor_k[i];
+            else NR[i-4]=xor_k[i];
+        }
 
-    int s_out[4]={S0[r1][c1]/2,S0[r1][c1]%2,S1[r2][c2]/2,S1[r2][c2]%2};
+        r1=NL[0]*2+NL[3]; c1=NL[1]*2+NL[2];
+        r2=NR[0]*2+NR[3]; c2=NR[1]*2+NR[2];
+        s_out[0]=S0[r1][c1]/2; s_out[1]=S0[r1][c1]%2;
+        s_out[2]=S1[r2][c2]/2; s_out[3]=S1[r2][c2]%2;
+        for(int i=0;i<4;i++) p4[i]=s_out[P4[i]-1]^L4[i];
 
-    int p4[4];
-    for(int i=0;i<4;i++) p4[i]=s_out[P4[i]-1];
-    for(int i=0;i<4;i++) p4[i]^=L4[i];
-
-    //Swap
-    for(int i=0;i<4;i++){
-        L4[i]=R4[i];
-        R4[i]=p4[i];
-    }
-
-    //fk with k1
-    for(int i=0;i<8;i++) new_EP[i]=R4[EP[i]-1];
-    for(int i=0;i<8;i++){
-        xor_k[i]=k1[i]^new_EP[i];
-        if(i<4) NL[i]=xor_k[i];
-        else NR[i-4]=xor_k[i];
-    }
-
-    r1=NL[0]*2+NL[3]; c1=NL[1]*2+NL[2];
-    r2=NR[0]*2+NR[3]; c2=NR[1]*2+NR[2];
-
-    s_out[0]=S0[r1][c1]/2; s_out[1]=S0[r1][c1]%2;
-    s_out[2]=S1[r2][c2]/2; s_out[3]=S1[r2][c2]%2;
-
-    for(int i=0;i<4;i++) p4[i]=s_out[P4[i]-1];
-    for(int i=0;i<4;i++) p4[i]^=L4[i];
-
-    int pre_PT[8];
-    for(int i=0;i<4;i++){
-        pre_PT[i]=p4[i];
-        pre_PT[i+4]=R4[i];
-    }
-
-    int PT[8];
-    for(int i=0;i<8;i++){
-        PT[i]=pre_PT[IP_inv[i]-1];
+        int pre[8];
+        for(int i=0;i<4;i++){ pre[i]=p4[i]; pre[i+4]=R4[i]; }
+        for(int i=0;i<8;i++) PT[pi++]=pre[IP_inv[i]-1];
     }
 
     printf("Plain Text: ");
-    for(int i=0;i<8;i++) printf("%d",PT[i]);
+    for(int i=0;i<original_len;i++) printf("%d",PT[i]);
     printf("\n");
 
-    free(cipher);
-    closesocket(acceptConnection);
-    closesocket(socketStatus);
+    closesocket(client);
+    closesocket(server);
     WSACleanup();
-    clock_t end = clock();
-    double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-
-    printf("Time taken for computation: %f seconds\n", cpu_time_used);
     return 0;
 }
