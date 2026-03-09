d5b60f5bfadfb7ecc4eab800d83d413f38f9d6b1
Deepak-Ragavendra <deeppanbhu05@gmail.com>
2026-03-09 12:31:33 +0530

    Elgomal correction

--- Lab-8/client.c
+++ Lab-8/client.c
@@ -21,102 +21,20 @@ long long modexp(long long a,long long b,long long p)
     return res;
 }
 
-int isPrime(int n)
-{
-    int i;
-
-    if(n<=1) return 0;
-
-    for(i=2;i*i<=n;i++)
-        if(n%i==0)
-            return 0;
-
-    return 1;
-}
-
-int isPrimitiveRoot(int g,int p)
-{
-    int i;
-
-    for(i=1;i<p-1;i++)
-        if(modexp(g,i,p)==1)
-            return 0;
-
-    return 1;
-}
-
 int main()
 {
-    srand(time(NULL)); 
+    srand(time(NULL));
+
     WSADATA wsa;
     SOCKET s;
     struct sockaddr_in server;
 
-    long long p,g,x,h;
+    long long p,g,h;
     long long m1,m2;
     long long r1,r2;
 
     long long C11,C21,C12,C22;
     long long HC1,HC2;
-
-    printf("Enter prime p: ");
-    scanf("%lld",&p);
-
-    while(!isPrime(p))
-    {
-        printf("Enter prime again: ");
-        scanf("%lld",&p);
-    }
-
-    printf("Enter primitive root g: ");
-    scanf("%lld",&g);
-
-    if(!isPrimitiveRoot(g,p))
-    {
-        printf("g is not primitive root\n");
-        return 0;
-    }
-
-    x = rand()%(p-2)+1;
-
-    printf("Private key x = %lld\n",x);
-
-    h = modexp(g,x,p);
-
-    printf("Public key h = %lld\n",h);
-
-    printf("Enter message1: ");
-    scanf("%lld",&m1);
-
-    printf("Enter message2: ");
-    scanf("%lld",&m2);
-
-    r1 = rand()%5 + 1;
-    r2 = rand()%5 + 1;
-
-    printf("Nonce r1 = %lld\n",r1);
-    printf("Nonce r2 = %lld\n",r2);
-
-    /* Encryption */
-
-    C11 = modexp(g,r1,p);
-    C21 = (m1 * modexp(h,r1,p)) % p;
-
-    C12 = modexp(g,r2,p);
-    C22 = (m2 * modexp(h,r2,p)) % p;
-
-    printf("\nE(m1) = (%lld , %lld)\n",C11,C21);
-    printf("E(m2) = (%lld , %lld)\n",C12,C22);
-
-    /* Homomorphic Multiplication */
-
-    HC1 = (C11*C12)%p;
-    HC2 = (C21*C22)%p;
-
-    printf("\nHomomorphic Encryption:\n");
-    printf("E(m1)*E(m2) = (%lld , %lld)\n",HC1,HC2);
-
-    /* Socket */
 
     WSAStartup(MAKEWORD(2,2),&wsa);
 
@@ -128,11 +46,54 @@ int main()
 
     connect(s,(struct sockaddr *)&server,sizeof(server));
 
+    /* RECEIVE PUBLIC KEY */
+
+    recv(s,(char*)&p,sizeof(p),0);
+    recv(s,(char*)&g,sizeof(g),0);
+    recv(s,(char*)&h,sizeof(h),0);
+
+    printf("Received Public Key:\n");
+    printf("p = %lld\n",p);
+    printf("g = %lld\n",g);
+    printf("h = %lld\n",h);
+
+    printf("Enter message1: ");
+    scanf("%lld",&m1);
+
+    printf("Enter message2: ");
+    scanf("%lld",&m2);
+
+    r1 = rand()%(p-2)+1;
+    r2 = rand()%(p-2)+1;
+
+    printf("Nonce r1 = %lld\n",r1);
+    printf("Nonce r2 = %lld\n",r2);
+
+    /* ENCRYPTION */
+
+    C11 = modexp(g,r1,p);
+    C21 = (m1 * modexp(h,r1,p)) % p;
+
+    C12 = modexp(g,r2,p);
+    C22 = (m2 * modexp(h,r2,p)) % p;
+
+    printf("\nE(m1) = (%lld , %lld)\n",C11,C21);
+    printf("E(m2) = (%lld , %lld)\n",C12,C22);
+
+    /* HOMOMORPHIC MULTIPLICATION */
+
+    HC1 = (C11*C12)%p;
+    HC2 = (C21*C22)%p;
+
+    printf("\nHomomorphic Cipher:\n");
+    printf("E(m1)*E(m2) = (%lld , %lld)\n",HC1,HC2);
+
+    printf("Expected Result (m1*m2 mod p) = %lld\n",(m1*m2)%p);
+
+    /* SEND TO SERVER */
+
     send(s,(char*)&HC1,sizeof(HC1),0);
     send(s,(char*)&HC2,sizeof(HC2),0);
-
-    send(s,(char*)&x,sizeof(x),0);
-    send(s,(char*)&p,sizeof(p),0);
 
     closesocket(s);
     WSACleanup();
--- Lab-8/server.c
+++ Lab-8/server.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <winsock2.h>
+#include <time.h>
 #pragma comment(lib,"Ws2_32.lib")
 
 long long modexp(long long a,long long b,long long p)
@@ -24,6 +26,30 @@ long long modInverse(long long a,long long p)
     return modexp(a,p-2,p);
 }
 
+int isPrime(long long n)
+{
+    int i;
+
+    if(n<=1) return 0;
+
+    for(i=2;i*i<=n;i++)
+        if(n%i==0)
+            return 0;
+
+    return 1;
+}
+
+int isPrimitiveRoot(long long g,long long p)
+{
+    int i;
+
+    for(i=1;i<p-1;i++)
+        if(modexp(g,i,p)==1)
+            return 0;
+
+    return 1;
+}
+
 int main()
 {
     WSADATA wsa;
@@ -32,10 +58,11 @@ int main()
 
     int c;
 
+    long long p,g,x,h;
     long long HC1,HC2;
-    long long x,p;
-
     long long svalue,inv,m;
+
+    srand(time(NULL));
 
     WSAStartup(MAKEWORD(2,2),&wsa);
 
@@ -54,13 +81,48 @@ int main()
     c = sizeof(struct sockaddr_in);
     new_socket = accept(s,(struct sockaddr *)&client,&c);
 
+    /* INPUT FROM SERVER */
+
+    printf("Enter prime p: ");
+    scanf("%lld",&p);
+
+    while(!isPrime(p))
+    {
+        printf("Enter prime again: ");
+        scanf("%lld",&p);
+    }
+
+    printf("Enter primitive root g: ");
+    scanf("%lld",&g);
+
+    if(!isPrimitiveRoot(g,p))
+    {
+        printf("g is not primitive root\n");
+        return 0;
+    }
+
+    x = rand()%(p-2)+1;
+
+    printf("Private key x = %lld\n",x);
+
+    h = modexp(g,x,p);
+
+    printf("Public key h = %lld\n",h);
+
+    /* SEND PUBLIC KEY */
+
+    send(new_socket,(char*)&p,sizeof(p),0);
+    send(new_socket,(char*)&g,sizeof(g),0);
+    send(new_socket,(char*)&h,sizeof(h),0);
+
+    /* RECEIVE HOMOMORPHIC CIPHER */
+
     recv(new_socket,(char*)&HC1,sizeof(HC1),0);
     recv(new_socket,(char*)&HC2,sizeof(HC2),0);
 
-    recv(new_socket,(char*)&x,sizeof(x),0);
-    recv(new_socket,(char*)&p,sizeof(p),0);
-
     printf("\nReceived Cipher = (%lld , %lld)\n",HC1,HC2);
+
+    /* DECRYPTION */
 
     svalue = modexp(HC1,x,p);
 
